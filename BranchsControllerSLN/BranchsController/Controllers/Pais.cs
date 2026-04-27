ead83af7289ab5d264ccbf21a92a616ee5f48c73
carlosdsantos29-hash <carlos.d.santos29@aluno.senai.br>
2026-04-27 17:02:27 -0300

    PaísDetalhes

--- BranchsControllerSLN/BranchsController/Controllers/Pais.cs
+++ BranchsControllerSLN/BranchsController/Controllers/Pais.cs
@@ -8,5 +8,9 @@ namespace BranchsController.Controllers
         {
             return View();
         }
+        public IActionResult PaisDetalhes()
+        {
+            return View();
+        }
     }
 }
