15c4ad6a785b6c344173ccd79df7a3e75c6d8c2d
carlosdsantos29-hash <carlos.d.santos29@aluno.senai.br>
2026-04-27 15:49:55 -0300

    EstadoController

---
+++ BranchsControllerSLN/BranchsController/Controllers/EstadoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BranchsController.Controllers
+{
+    public class EstadoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
